b63f3ecf850a7f5729d013ce73b42b16912fdc7f
AdamsAppios <23veclipse23v@gmail.com>
2020-06-14 23:23:08 +0800

    Changes to models.py cctvcounter

--- businessreport/static/staticreport/ref_rep.js
+++ businessreport/static/staticreport/ref_rep.js
@@ -1,1 +1,32 @@
-$(document).read(function () {});
+$(document).read(function () {
+  function ReportClass() {
+    this.dealPrice = 10;
+    this.pickPrice = 15;
+    this.rndPrice = 150;
+  }
+  function LabReportClass() {
+    ReportClass.call(this);
+    this.squarePrice = 15;
+    this.smallPrice = 8;
+    this.goodlyPrice = 8;
+  }
+
+  function ReportClass() {
+    this.dealPrice = 10;
+    this.pickPrice = 15;
+    this.rndPrice = 150;
+  }
+  function LabReportClass() {
+    ReportClass.call(this);
+    this.squarePrice = 15;
+    this.smallPrice = 8;
+    this.goodlyPrice = 8;
+  }
+  function TmbReportClass() {
+    ReportClass.call(this);
+    this.dealPrice = 8;
+    this.pickPrice = 12;
+  }
+  LabReportClass.prototype = Object.create(ReportClass.prototype);
+  TmbReportClass.prototype = Object.create(ReportClass.prototype);
+});
