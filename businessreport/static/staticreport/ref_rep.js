9be405f924a13e0e683a5b6f2c7b298a7933cb19
AdamsAppios <23veclipse23v@gmail.com>
2020-06-14 23:41:04 +0800

    Renamed project folder

--- businessreport/static/staticreport/ref_rep.js
+++ businessreport/static/staticreport/ref_rep.js
@@ -34,7 +34,44 @@ $(document).read(function () {
     this.smallSqPrice = 10;
     this.bakeryPrice = 15;
   }
+
   LabReportClass.prototype = Object.create(ReportClass.prototype);
   TmbReportClass.prototype = Object.create(ReportClass.prototype);
   KalimpReportClass.prototype = Object.create(ReportClass.prototype);
+  LabReportClass.prototype.updateCTOCalc = function () {
+    var ctoCalc =
+      (parseInt($("#dealText").val()) - parseInt($("#less").val())) *
+        this.dealPrice +
+      parseInt($("#pickText").val()) * this.pickPrice +
+      parseInt($("#goodlyText").val()) * this.goodlyPrice +
+      parseInt($("#smallText").val()) * this.smallPrice +
+      parseInt($("#squareText").val()) * this.squarePrice +
+      parseInt($("#rndText").val()) * this.rndPrice -
+      parseInt($("#totalExpenses").html()) -
+      parseInt($("#cashTkn").val());
+    $("#ctoCalc").val(ctoCalc);
+  };
+  TmbReportClass.prototype.updateCTOCalc = function () {
+    var ctoCalc =
+      parseInt($("#dealText").val()) * this.dealPrice +
+      parseInt($("#pickText").val()) * this.pickPrice +
+      parseInt($("#rndText").val()) * this.rndPrice -
+      parseInt($("#totalExpenses").html()) -
+      parseInt($("#cashTkn").val());
+    $("#ctoCalc").val(ctoCalc);
+  };
+  KalimpReportClass.prototype.updateCTOCalc = function () {
+    var ctoCalc =
+      (parseInt($("#dealText").val()) - parseInt($("#less").val())) *
+        this.dealPrice +
+      parseInt($("#pickText").val()) * this.pickPrice +
+      parseInt($("#smallSquare").val()) * this.smallSqPrice +
+      parseInt($("#bakeryGal").val()) * this.bakeryPrice +
+      parseInt($("#smallText").val()) * this.smallPrice +
+      parseInt($("#squareText").val()) * this.squarePrice +
+      parseInt($("#rndText").val()) * this.rndPrice -
+      parseInt($("#totalExpenses").html()) -
+      parseInt($("#cashTkn").val());
+    $("#ctoCalc").val(ctoCalc);
+  };
 });
