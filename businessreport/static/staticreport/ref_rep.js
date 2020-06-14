e812458c98e7fa5c0dea4cd51d135f49ec6f2459
AdamsAppios <23veclipse23v@gmail.com>
2020-06-14 23:32:34 +0800

    Modified settings.py to change project name

--- businessreport/static/staticreport/ref_rep.js
+++ businessreport/static/staticreport/ref_rep.js
@@ -27,6 +27,14 @@ $(document).read(function () {
     this.dealPrice = 8;
     this.pickPrice = 12;
   }
+  function KalimpReportClass() {
+    ReportClass.call(this);
+    this.squarePrice = 15;
+    this.smallPrice = 5;
+    this.smallSqPrice = 10;
+    this.bakeryPrice = 15;
+  }
   LabReportClass.prototype = Object.create(ReportClass.prototype);
   TmbReportClass.prototype = Object.create(ReportClass.prototype);
+  KalimpReportClass.prototype = Object.create(ReportClass.prototype);
 });
--- django_helpersite/settings.py
+++ django_helpersite/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'django_helpersite.urls'
+ROOT_URLCONF = 'arso_helpersite.urls'
 
 TEMPLATES = [
     {
@@ -74,7 +74,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'django_helpersite.wsgi.application'
+WSGI_APPLICATION = 'arso_helpersite.wsgi.application'
 
 
 # Database
